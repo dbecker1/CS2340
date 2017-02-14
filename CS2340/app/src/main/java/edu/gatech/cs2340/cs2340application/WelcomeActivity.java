8711deae4eb076d6f6b43bcc964a5c309bb84750
cliffpanos <cliffpanos@gmail.com>
2017-02-13 22:10:36 -0500

    Cliff fixed a bug and worked on activity welcome

--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/WelcomeActivity.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/WelcomeActivity.java
@@ -1,13 +1,37 @@
 package edu.gatech.cs2340.cs2340application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.view.View;
+
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        loginButton = (Button) findViewById(R.id.login_button);
+        registerButton = (Button) findViewById(R.id.register_button);
+    }
+
+    protected void onLoginPressed(View view) {
+
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
+
+    }
+
+    protected void onRegisterPressed(View view) {
+
+        Intent register = new Intent(this, LoginActivity.class);
+        startActivity(register);
+
     }
 }
