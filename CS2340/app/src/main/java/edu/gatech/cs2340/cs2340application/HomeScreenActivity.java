8cc9ea485ff956f4e8454b01c38e0e3a3bc52145
Daniel <dbecker.fl@gmail.com>
2017-02-13 22:06:55 -0500

    Modified HomeScreen

--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/HomeScreenActivity.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/HomeScreenActivity.java
@@ -1,13 +1,31 @@
 package edu.gatech.cs2340.cs2340application;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_homescreen);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Logout"};
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 }
