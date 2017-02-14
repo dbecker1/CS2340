1a80934619dc5a38c5bf43b36245398b6d5b6758
Daniel <dbecker.fl@gmail.com>
2017-02-13 21:41:03 -0500

    Modified LoginActivity

--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/LoginActivity.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/LoginActivity.java
@@ -1,13 +1,40 @@
 package edu.gatech.cs2340.cs2340application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView errorView;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_login);
+        errorView = (TextView) findViewById(R.id.errorView);
+        username = (EditText) findViewById(R.id.usernameTextField);
+        password = (EditText) findViewById(R.id.passwordTextField);
     }
+
+    protected void onSubmitPressed(View view) {
+        if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")){
+            Intent next = new Intent(this, HomeScreenActivity.class);
+            startActivity(next);
+        } else {
+            errorView.setText("Error: Incorrect Username or Password");
+        }
+    }
+
+    protected void onCancelPressed(View view) {
+        Intent back = new Intent(this, WelcomeActivity.class);
+        startActivity(back);
+    }
+
 }
