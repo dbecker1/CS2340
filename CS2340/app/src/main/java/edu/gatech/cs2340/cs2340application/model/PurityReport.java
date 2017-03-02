5775937ab72aa6c1c7bcf62f2901c38a47e85a1b
Kathy Tran <ktran77@gatech.edu>
2017-03-01 20:18:21 -0500

    Added javadocs for some classes in model

--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/PurityReport.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/PurityReport.java
@@ -11,26 +11,58 @@ public class PurityReport extends Report {
     private Double virusPPM;
     private Double contaminantPPM;
 
+    /**
+     * Return the condition attribute contained inside a water purity report
+     *
+     * @return the condition of the water reported in a water purity report
+     */
     public String getCondition() {
         return condition;
     }
 
+    /**
+     * Set the condition for the water being reported in a water purity report
+     *
+     * @param condition The condition of water: Waste, Treatable-Clear, Treatable-Muddy, Potable
+     */
     public void setCondition(String condition) {
         this.condition = condition;
     }
 
+    /**
+     * Return the Virus PPM data of a water purity report
+     * @return The number of virus in parts-per-million measuring how much virus there are in the
+     * water
+     */
     public Double getVirusPPM() {
         return virusPPM;
     }
 
+    /**
+     * Set the virus PPM data for a water purity report
+     *
+     * @param virusPPM The number of virus in parts-per-million measuring how much virus there are
+     * in the water
+     */
     public void setVirusPPM(Double virusPPM) {
         this.virusPPM = virusPPM;
     }
 
+    /**
+     * Return the contaminant PPM data of a water purity report.
+     * @return the number of contaminant in parts-per-million measuring how much contaminant there
+     * are in the water
+     */
     public Double getContainmentPPM() {
         return contaminantPPM;
     }
 
+    /**
+     * Set the contaminant PPM data for a water purity report
+     *
+     * @param containmentPPM The number of contaminant in parts-per-million measuring how much
+     * contaminant there are in the water
+     */
     public void setContainmentPPM(Double containmentPPM) {
         this.contaminantPPM = containmentPPM;
     }
--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/Report.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/Report.java
@@ -12,35 +12,73 @@ public class Report {
     private String userId;
     private String location;
 
-
+    /**
+     * Returns the report number of a water report
+     *
+     * @return the report number of a water report
+     */
     public String getReportNumber() {
         return reportNumber;
     }
 
+    /**
+     * Set the report number for a water report
+     * @param reportNumber String representation of a water report number
+     */
     public void setReportNumber(String reportNumber) {
         this.reportNumber = reportNumber;
     }
 
+    /**
+     * Return the date and time of a water report
+     *
+     * @return the date and time attributes of a water report
+     */
     public Date getDateTime() {
         return dateTime;
     }
 
+    /**
+     * Set the date and time attribute of a water report
+     *
+     * @param dateTime the date and time that of when the report is written/created
+     */
     public void setDateTime(Date dateTime) {
         this.dateTime = dateTime;
     }
 
+    /**
+     * Return the User ID of the User who submitted/written the report
+     *
+     * @return the String representation of the User ID of the user that submitted the report
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Set the User ID of the user who submitted the report
+     *
+     * @param userId a String containing the User ID of the User who submitted the report
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     * Return the location of the water that was being reported on in the water reported
+     *
+     * @return a String representation of the location of the water being reported.
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Set the location of the water that was being reported on in the water reported
+     *
+     * @param location a String representation of the location of the water being reported.
+     */
     public void setLocation(String location) {
         this.location = location;
     }
