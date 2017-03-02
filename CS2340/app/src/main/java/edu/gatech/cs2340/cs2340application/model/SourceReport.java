864de1f5ba21574c47729af082a3bc629d37271d
Kathy Tran <ktran77@gatech.edu>
2017-03-01 21:49:37 -0500

    Finished adding the javadocs for all the classes

--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/SourceReport.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/SourceReport.java
@@ -11,18 +11,39 @@ public class SourceReport extends Report {
     private String type;
     private String condition;
 
+    /**
+     * Returns a String representation of the type of water being reported in the water source
+     * report
+     *
+     * @return a String representation of the type of water being reported.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of water being reported in the water source report.
+     *
+     * @param type a String representation of the type of water reported.
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Returns the condition of the water from the water report
+     *
+     * @return a String representation of the condition of the water from the water report
+     */
     public String getCondition() {
         return condition;
     }
 
+    /**
+     * Sets the condition of the water for the water report
+     *
+     * @param condition a String representation of the condition of the water from the water report
+     */
     public void setCondition(String condition) {
         this.condition = condition;
     }
--- CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/User.java
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/model/User.java
@@ -17,36 +17,76 @@ public class User {
     protected String userType;
     protected String address;
 
+    /**
+     * Overloading with a constructor of the User class with no assignment to the instance
+     * variables
+     */
     public User() {
-
     }
 
+    /**
+     * Creates a user class with the user's email address, account type, and address
+     *
+     * @param emailAddress a String representation of the user's email address
+     * @param userType a String representation of the user account type
+     * @param address a String representation of the user's address
+     */
     public User(String emailAddress, String userType, String address) {
         this.emailAddress = emailAddress;
         this.userType = userType;
         this.address = address;
     }
 
+    /**
+     * Returns the user's email address
+     *
+     * @return a String representation of the user's email address
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     * Sets the user's email address
+     *
+     * @param emailAddress a String representation of the user's email address
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     * Returns the account type of the user
+     *
+     * @return A String representation of the the user account type
+     */
     public String getUserType() {
         return userType;
     }
 
+    /**
+     * Sets the account type of the user
+     *
+     * @param userType a String representation of the the user account type
+     */
     public void setUserType(String userType) {
         this.userType = userType;
     }
 
+    /**
+     * Returns the address of the user
+     *
+     * @return a String representation of the user's address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Sets the address of the user
+     *
+     * @param address a String representation of the user's address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
