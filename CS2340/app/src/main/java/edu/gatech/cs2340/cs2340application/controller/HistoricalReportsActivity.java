25d0c574cb95579d87c74347f3b7e4de88dd4615
Bryan <bzhang57@gmail.com>
2017-03-27 20:37:16 -0400

    Added graph activity

---
+++ CS2340/app/src/main/java/edu/gatech/cs2340/cs2340application/controller/HistoricalReportsActivity.java
@@ -0,0 +1,59 @@
+package edu.gatech.cs2340.cs2340application.controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.app.Activity;
+import android.graphics.*;
+import android.os.Bundle;
+
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.*;
+
+import edu.gatech.cs2340.cs2340application.R;
+
+public class HistoricalReportsActivity extends AppCompatActivity {
+
+    private XYPlot plot;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historical_reports);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // initialize our XYPlot reference:
+        plot = (XYPlot) findViewById(R.id.plot);
+
+        // create a couple arrays of y-values to plot:
+        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
+        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
+        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
+
+    }
+
+
+
+
+
+}
